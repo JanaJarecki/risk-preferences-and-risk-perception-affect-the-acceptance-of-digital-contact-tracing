17ebbc96799d8f7924c7cddb17ee62eed1e81e6f
Jana Jarecki <jarecki.jana@gmail.com>
2020-09-26 00:02:11 +0200

    new figure with demographics

--- analyses/code/1-demographics.R
+++ analyses/code/1-demographics.R
@@ -1,8 +1,6 @@
 # Setup --------------------------------------------------------------------
 if (!require(pacman)) install.packages("pacman")
-# Load packages and install missing packages in one step
 pacman::p_load(data.table)
-# set working directory to THIS file location if rstudio
 if (rstudioapi::isAvailable()) { setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) }
 
 
@@ -14,6 +12,7 @@ n_excluded <- 91  # see exclusion report in pre-process
 d$age <- as.numeric(d$age)
 d$gender <- factor(d$female, levels = c(0,1,2), labels = c("male", "female", "NA"))
 
+# Print demographics ---------------------------------------------------------
 cognitiveutils::participants(d,
   id="id",
   age="age",
@@ -22,3 +21,21 @@ cognitiveutils::participants(d,
   date = "enddate",
   recruitedfrom ="professional panel provider",
   approvedby = "the ethics committee of the faculty of Psychology at the University of Basel")
+
+# Table demographics ----------------------------------------------------------
+digits <- 2
+d[, female_n := ifelse(female==2, NA, female)]
+d[, high_school := education >= 4]
+tab <- d[, .(
+  Variable = c("Female", "Age", "At least high-school degree", "Household size", "Number of children", "Monthly net income (sFr)"),
+  Mdn = sapply(.SD, median, na.rm=T),
+  Mean = sapply(.SD, mean, na.rm=T),
+  SD = sapply(.SD, sd, na.rm=T),
+  Nonresponse = sapply(.SD, function(x) sum(is.na(x)))
+  ),
+  .SDcols = c("female_n", "age", "high_school", "household", "household_kids", "income_imputed")]
+
+knitr::kable(tab,
+  format = "latex",
+  booktabs = TRUE,
+  digits = c(2, 2, 2, 0))
