fae876e3b8c107c51074c572f05365c66ae89e5e
rebecca-albrecht <64608407+rebecca-albrecht@users.noreply.github.com>
2020-09-30 13:08:55 +0200

    Changed model estimation to do variable selection for submodels with set penalty.

--- analyses/code/2-statistical-model-estimation.R
+++ analyses/code/2-statistical-model-estimation.R
@@ -2,7 +2,7 @@
 # Statistical Modeling
 # Author: Jana B. Jarecki
 # ==========================================================================
-if (!require(pacman)) install.packages("pacman")
+if (!require(pacman)) tall.packages("pacman")
 pacman::p_load(data.table, brms, projpred, bayesplot, standardize, mice)
 # set working directory to THIS file location (if rstudio)
 if (rstudioapi::isAvailable()) { setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) }
@@ -38,17 +38,13 @@ indep_vars <- c("perc_risk_health", "perc_risk_data", "perc_risk_econ", "seek_ri
 contr_vars <- c("safebehavior_score", "know_health_score", "know_econ", "female", "age", "education", "community_imputed", "household", "was_infected", "is_infected", "has_symptoms", "income_imputed", "wealth_imputed", "has_work", "income_loss", "homeoffice", "policy_score", "mhealth_score", "tech_score", "compreh_score")
 d <- d[, .SD, .SDcols = c(dep_var, indep_vars, contr_vars)]
 
-
-
 # Standardize variables -------------------------------------------------------
-formula <- reformulate(
-  termlabels = c(indep_vars, contr_vars),
-  response = dep_var)
-sobj <- standardize(formula = formula, d)
+#formula <- reformulate(
+#  termlabels = c(indep_vars, contr_vars),
+#  response = dep_var)
+#sobj <- standardize(formula = formula, d)
 # Important:
 # 'sobj$data' must be used as data from here on
-
-
 
 # Variable selection procedure -----------------------------------------------
 # using leave-one-out cross-validation and Lasso (L1) penalization
@@ -59,10 +55,14 @@ nc <- ncol(sobj$data) # 31
 p0 <- length(indep_vars) # prior guess: number of relevant variables
 tau0 <- p0/(nc-p0) * 1/sqrt(n) # scale for tau (stan_glm scales this by sigma)
 prior_coeff <- set_prior(horseshoe(scale_global = tau0, scale_slab = 1)) # regularized horseshoe prior
-
+#prior_coeff <- set_prior("normal(0,10)", class = "b") # regularized horseshoe prior
 
 # 2. Fit full model
 # If you want to re-fit the model, run next line
+formula <- reformulate(
+  termlabels = c(indep_vars, contr_vars),
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
 # file.remove(paste0("fitted_models/", dep_var, "_fit_full.rds"))
 fit <- brm(formula = formula, family = gaussian(), data = sobj$data,
   prior = prior_coeff,
@@ -74,47 +74,145 @@ fit <- brm(formula = formula, family = gaussian(), data = sobj$data,
   ),
   file = paste0("fitted_models/", dep_var, "_fit_full"))
 
-
-
 # Variable selection ----------------------------------------------------------
 # Penalty of 0 = var is selected first, Inf = var is never selected
 #   * 0 for independent vars that are theoretically motivated
 #   * 1 for control vars
 betas <- grep("^b", parnames(fit), value=TRUE)[-1]
 penalty <- rep(1, length(betas))
-penalty[match(indep_vars, gsub("b_", "", betas))] <- 0
+penalty[match(c(indep_vars), gsub("b_", "", betas))] <- 0
 # betas[penalty==0] # check var penalty: ok
 cvs <- cv_varsel(fit, method = "L1", penalty = penalty)
-saveRDS(cvs, paste0("fitted_models/", dep_var, "_variable_selection.rds"))
+saveRDS(cvs, paste0("fitted_models/", dep_var, "_variable_selection_all.rds"))
 # Optional
 # Forward search & Lasso L1-penalty to find variable order (Tran et al., 2012)
 # vs <- varsel(fit,
 #   method = "L1",
 #   penalty = penalty)
 # vs$vind # variables ordered as they enter during the search
+# Fit reduced model
+cvs <- readRDS(paste0("fitted_models/", dep_var, "_variable_selection_all.rds"))
+nvar <- suggest_size(cvs)
+predictors_selected <- names(cvs$vind[1:nvar])
+formula <- reformulate(
+  termlabels = predictors_selected,
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
+#file.remove(paste0("fitted_models/", dep_var, "_fit_reduced_all.rds"))
+fit_reduced_all <- brm(formula = formula, family = gaussian(), data = sobj$data,
+                       prior = prior_coeff,
+                       save_all_pars = TRUE, sample_prior = "yes",
+                       iter = 8000,
+                       seed = 42,
+                       control = list(
+                         adapt_delta = 0.9 # because horseshoe prior prone to divergent transitions
+                       ),
+                       file = paste0("fitted_models/", dep_var, "_fit_reduced_all"))
 
-
-
-# Excluding the social variables ---------------------------------------------
-# Update reference model
-# file.remove(paste0("fitted_models/", dep_var, "_fit_no_social.rds"))
-fit2 <- update(fit,
-  formula. = ~ . - honhum_score - svo_angle - iwah_diff_score,
-  file = paste0("fitted_models/", dep_var, "_fit_no_social"))
-# Update variable selection with penalty
-betas <- grep("^b", parnames(fit2), value=TRUE)[-1]
+# Variable selection no social ----------------------------------------------------------
+betas <- grep("^b", parnames(fit), value=TRUE)[-1]
 penalty <- rep(1, length(betas))
-penalty[match(indep_vars, gsub("b_", "", betas))] <- 0
-cvs <- cv_varsel(fit2, method = "L1", penalty = penalty)
-saveRDS(cvs,paste0("fitted_models/",dep_var,"_variable_selection_no_social.rds"))
+penalty[match(indep_vars[1:7], gsub("b_", "", betas))] <- 0
+cvs <- cv_varsel(fit, method = "L1", penalty = penalty)
+saveRDS(cvs, paste0("fitted_models/", dep_var, "_variable_selection_no_social.rds"))
+# Fit reduced model
+cvs <- readRDS(paste0("fitted_models/", dep_var, "_variable_selection_no_social.rds"))
+nvar <- suggest_size(cvs)
+predictors_selected <- names(cvs$vind[1:nvar])
+formula <- reformulate(
+  termlabels = predictors_selected,
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
+#file.remove(paste0("fitted_models/", dep_var, "_fit_reduced_no_social.rds"))
+fit_reduced_no_social <- brm(formula = formula, family = gaussian(), data = sobj$data,
+                       prior = prior_coeff,
+                       save_all_pars = TRUE, sample_prior = "yes",
+                       iter = 8000,
+                       seed = 42,
+                       control = list(
+                         adapt_delta = 0.9 # because horseshoe prior prone to divergent transitions
+                       ),
+                       file = paste0("fitted_models/", dep_var, "_fit_reduced_no_social"))
+
+# Variable selection no risk seek ----------------------------------------------------------
+betas <- grep("^b", parnames(fit), value=TRUE)[-1]
+penalty <- rep(1, length(betas))
+penalty[match(c(indep_vars[1:3], indep_vars[8:10]), gsub("b_", "", betas))] <- 0
+cvs <- cv_varsel(fit, method = "L1", penalty = penalty)
+saveRDS(cvs, paste0("fitted_models/", dep_var, "_variable_selection_no_riskseek.rds"))
+# Fit reduced model
+cvs <- readRDS(paste0("fitted_models/", dep_var, "_variable_selection_no_riskseek.rds"))
+nvar <- suggest_size(cvs)
+predictors_selected <- names(cvs$vind[1:nvar])
+formula <- reformulate(
+  termlabels = predictors_selected,
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
+#file.remove(paste0("fitted_models/", dep_var, "_fit_reduced_no_riskseek.rds"))
+fit_reduced_no_riskseek <- brm(formula = formula, family = gaussian(), data = sobj$data,
+                             prior = prior_coeff,
+                             save_all_pars = TRUE, sample_prior = "yes",
+                             iter = 8000,
+                             seed = 42,
+                             control = list(
+                               adapt_delta = 0.9 # because horseshoe prior prone to divergent transitions
+                             ),
+                             file = paste0("fitted_models/", dep_var, "_fit_reduced_no_riskseek"))
+
+# Variable selection no social ----------------------------------------------------------
+betas <- grep("^b", parnames(fit), value=TRUE)[-1]
+penalty <- rep(1, length(betas))
+penalty[match(indep_vars[1:7], gsub("b_", "", betas))] <- 0
+cvs <- cv_varsel(fit, method = "L1", penalty = penalty)
+saveRDS(cvs, paste0("fitted_models/", dep_var, "_variable_selection_no_social.rds"))
+# Fit reduced model
+cvs <- readRDS(paste0("fitted_models/", dep_var, "_variable_selection_no_social.rds"))
+nvar <- suggest_size(cvs)
+predictors_selected <- names(cvs$vind[1:nvar])
+formula <- reformulate(
+  termlabels = predictors_selected,
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
+#file.remove(paste0("fitted_models/", dep_var, "_fit_reduced_no_social.rds"))
+fit_reduced_no_social <- brm(formula = formula, family = gaussian(), data = sobj$data,
+                             prior = prior_coeff,
+                             save_all_pars = TRUE, sample_prior = "yes",
+                             iter = 8000,
+                             seed = 42,
+                             control = list(
+                               adapt_delta = 0.9 # because horseshoe prior prone to divergent transitions
+                             ),
+                             file = paste0("fitted_models/", dep_var, "_fit_reduced_no_social"))
+
+# Variable selection no risk perception ----------------------------------------------------------
+betas <- grep("^b", parnames(fit), value=TRUE)[-1]
+penalty <- rep(1, length(betas))
+penalty[match(indep_vars[4:10], gsub("b_", "", betas))] <- 0
+cvs <- cv_varsel(fit, method = "L1", penalty = penalty)
+saveRDS(cvs, paste0("fitted_models/", dep_var, "_variable_selection_no_riskperc.rds"))
+# Fit reduced model
+cvs <- readRDS(paste0("fitted_models/", dep_var, "_variable_selection_no_riskperc.rds"))
+nvar <- suggest_size(cvs)
+predictors_selected <- names(cvs$vind[1:nvar])
+formula <- reformulate(
+  termlabels = predictors_selected,
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
+#file.remove(paste0("fitted_models/", dep_var, "_fit_reduced_no_riskperc.rds"))
+fit_reduced_no_riskperc <- brm(formula = formula, family = gaussian(), data = sobj$data,
+                               prior = prior_coeff,
+                               save_all_pars = TRUE, sample_prior = "yes",
+                               iter = 8000,
+                               seed = 42,
+                               control = list(
+                                 adapt_delta = 0.9 # because horseshoe prior prone to divergent transitions
+                               ),
+                               file = paste0("fitted_models/", dep_var, "_fit_reduced_no_riskperc"))
 
 
-
-
-
-
-
-
+BF_all_no_social = bayes_factor(fit_reduced_all, fit_reduced_no_social)
+BF_all_no_riskseek = bayes_factor(fit_reduced_all, fit_reduced_no_riskseek)
+BF_all_no_riskperc = bayes_factor(fit_reduced_all, fit_reduced_no_riskperc)
 
 
 # ==========================================================================
--- analyses/code/3-statistical-analysis-main-effects.R
+++ analyses/code/3-statistical-analysis-main-effects.R
@@ -1,17 +1,17 @@
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse, data.table, brms, projpred, standardize)
 # set working directory to THIS file location (if rstudio)
 if (rstudioapi::isAvailable()) { setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) }
 
 
 # Load data ---------------------------------------------------------------
 d <- fread("../../data/processed/data.csv")
-
+d$seek_risk_data
 cor.test(d$accept_index, d$seek_risk_data)
 cor.test(d$accept_index, d$seek_risk_health)
 cor.test(d$accept_index, d$perc_risk_data)
 cor.test(d$accept_index, d$perc_risk_health)
-
+cor.test(d$policy_score, d$perc_risk_econ)
 
 d_tibble <- as_tibble(d) %>% 
   mutate(seek_risk_data_z = (seek_risk_data - mean(seek_risk_data))/sd(seek_risk_data),
@@ -31,7 +31,32 @@ d_tibble <- d_tibble %>%
           name_1 = ifelse(name_1 == "perc", "Risk Perception", "Risk Seeking Preference"),
           Domain = ifelse(Domain == "data", "Data", "Health"))
 
-ggplot(data=d_tibble, aes(x=accept_index, y=value, color=Domain)) + geom_smooth(method=lm) +
-    facet_wrap(~name_1) + labs(x = "Acceptance", y="Z-score")
+ggplot(data=d_tibble, aes(x=accept_index, y=value, color=Domain)) + 
+  geom_smooth(method=lm) + #fill = "grey85"
+  facet_wrap(~name_1) + 
+  #scale_colour_manual(values = c("grey0", "grey50")) +
+  labs(x = "Acceptance", y="Z-score") +
+  theme_classic() 
 
+### Compare projection and fitted model
+dep_var <- "accept_index"
+cvs <- readRDS(paste0("fitted_models/", dep_var, "_variable_selection.rds"))
+nvar <- suggest_size(cvs)
+proj <- project(cvs, nv = nvar, ns = 1000)
 
+predictors_selected <- names(cvs$vind[1:nvar])
+formula <- reformulate(
+  termlabels = predictors_selected,
+  response = dep_var)
+sobj <- standardize(formula = formula, d)
+fit_reduced <- brm(formula = formula, family = gaussian(), data = sobj$data,
+                       prior = prior_coeff,
+                       save_all_pars = TRUE, sample_prior = "yes",
+                       iter = 8000,
+                       seed = 42,
+                       control = list(
+                         adapt_delta = 0.9 # because horseshoe prior prone to divergent transitions
+                       ),
+                       file = paste0("fitted_models/", dep_var, "_fit_reduced_all"))
+
+proj$beta
